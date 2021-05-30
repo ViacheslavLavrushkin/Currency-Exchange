ecfb039c7b6c6fbffa7fbf52a026bb903bb29e1b
Viach92 <lavrushkinvv@gmail.com>
2021-05-31 01:03:14 +0300

    HomeWork5

--- app/currency/models.py
+++ app/currency/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Rate(models.Model):
+    type1 = models.CharField(max_length=5)
+    sale = models.DecimalField(max_digits=5, decimal_places=2)
+    buy = models.DecimalField(max_digits=5, decimal_places=2)
+    created = models.DateTimeField(auto_now_add=True)
+    source = models.CharField(max_length=64)
+
+
+class ContactUS(models.Model):
+    email_from = models.EmailField(max_length=64)
+    subject = models.CharField(max_length=5)
+    message = models.CharField(max_length=5)
